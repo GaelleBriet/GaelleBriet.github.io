0994e358554f632b9e2301fbb96eee565a549968
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-21 20:15:00 +0200

    views && NavigationComponent.vue

---
+++ app/app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HomeView from '~/Views/HomeView.vue'
+import NavigationComponent from '~/Components/NavigationComponent.vue'
+</script>
+
+<template>
+  <div>
+    <!--    <NuxtRouteAnnouncer />-->
+    <NavigationComponent />
+    <!--    <NuxtWelcome /> -->
+    <HomeView />
+  </div>
+</template>
+<style>
+body {
+  @apply bg-gray-100 dark:bg-gray-900;
+}
+</style>
