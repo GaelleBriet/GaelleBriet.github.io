41bec621aae3b6fa0bf5b8684d17f693b726c6ee
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-21 17:44:18 +0200

    icons config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,8 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/icon"],
   colorMode: {
     preference: 'light'
-  }
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      'postcss-nested': {},
+      "postcss-import": {},
+      "tailwindcss/nesting": {},
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
 })
