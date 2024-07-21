9a0221804a1be247ac1c2ba2ddb2de456c04328b
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-21 22:50:11 +0200

    home page design

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,69 +1,15 @@
-<script setup lang="ts">
-const route = useRoute()
-
-const links = [
-  {
-    label: 'Home',
-    icon: 'i-heroicons-home',
-    to: '/',
-  },
-  {
-    label: 'About',
-    icon: 'i-heroicons-information-circle',
-    to: '/about',
-  },
-  {
-    label: 'Projects',
-    icon: 'i-heroicons-folder-open',
-    to: '/projects',
-  },
-  {
-    label: 'Contact',
-    icon: 'i-heroicons-envelope',
-    to: '/contact',
-  },
-]
-
-const linksRight = [
-  {
-    label: 'GitHub',
-    icon: 'i-heroicons-arrow-top-right-on-square',
-  },
-  {
-    label: 'Linkedin',
-    icon: 'i-heroicons-arrow-top-right-on-square',
-  },
-]
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <div class="flex justify-around">
-      <div class="flex row">
-        <UHorizontalNavigation
-          :links="links"
-          class="border-b border-gray-200 dark:border-gray-800 flex justify-center"
-        />
-      </div>
-      <div class="flex row">
-        <UHorizontalNavigation
-          :links="linksRight"
-          class="border-b border-gray-200 dark:border-gray-800 flex justify-center"
-        />
-        <div class="flex items-center">
-          <UButton
-            :ui="{ rounded: 'rounded-2xl' }"
-            label="button"
-            size="sm"
-            variant="outline"
-            icon="i-heroicons-arrow-down-tray"
-            trailing
-            class="px-3 py-1.5"
-          ></UButton>
-        </div>
-      </div>
-      <!--    <UBadge>Mon badge</UBadge>-->
+  <div class="h-screen flex flex-col mx-20">
+    <NavigationComponent class="h-12" />
+    <div class="flex-1 overflow-auto">
+      <slot />
     </div>
-    <slot />
   </div>
 </template>
+<style>
+.flex-1 {
+  height: calc(100vh - 3rem);
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const cardConfig = {
+  divide: 'divide-y-0',
+  body: {
+    padding: 'p-4', // Ajuste le padding si nécessaire
+  },
+  header: {
+    padding: 'px-4 pt-4 pb-0', // Ajuste le padding du header
+  },
+  footer: {
+    padding: 'px-4 pb-4 pt-0', // Ajuste le padding du footer
+  },
+}
+</script>
 
 <template>
-  <h1>Bienvenue sur la page d'accueil</h1>
-  <NuxtLink to="/about">À propos</NuxtLink>
+  <div class="h-full w-full flex flex-col">
+    <div class="flex flex-col justify-center items-center h-full">
+      <div class="flex flex-row w-full justify-center h-80">
+        <div class="me-4 w-1/3 rounded-lg overflow-hidden p-0">
+          <img
+            class="object-cover w-full h-full"
+            src="~/assets/img/portrait.jpeg"
+            alt="Discover Nuxt 3"
+          />
+        </div>
+        <UCard class="w-2/3 flex flex-col h-full" :ui="cardConfig">
+          <template #header>
+            <div>
+              <p class="text-sm text-gray-500">dev</p>
+              <h1 class="text-xl font-bold">name</h1>
+            </div>
+          </template>
+          <div class="flex-grow overflow-auto h-36">
+            <!-- Cette div prendra tout l'espace disponible -->
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cupiditate
+              deleniti ex explicabo hic ipsum iure magnam maxime mollitia natus, nesciunt obcaecati
+              placeat quis ratione tempora, ullam vel velit voluptas.
+            </p>
+          </div>
+
+          <template #footer>
+            <div class="flex justify-start space-x-3">
+              <UButton
+                :ui="{ rounded: 'rounded-3xl' }"
+                label="contact"
+                size="sm"
+                variant="solid"
+                trailing
+                class="px-5 py-2.5"
+              ></UButton>
+              <UButton
+                :ui="{ rounded: 'rounded-3xl' }"
+                label="projects"
+                size="sm"
+                variant="outline"
+                trailing
+                class="px-5 py-2.5"
+              ></UButton>
+            </div>
+          </template>
+        </UCard>
+      </div>
+    </div>
+  </div>
 </template>
